e5a4107798705897c57f767100c87fc9182ec4d6
patelpavitra369 <patelpavitra369@gmail.com>
2022-04-09 11:51:05 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// function calculation(a, b, type) {
-//   if (type == 'sum') {
-//     return a + b;
-//   } else if (type == 'sub') {
-//     return a - b;
-//   } else if (type == 'mul') {
-//     return a * b;
-//   }
-// }
-// console.log(calculation(1, 2, 'sum'));
-// console.log(calculation(1, 2, 'sub'));
-// console.log(calculation(1, 2, 'mul'));
-
 // var aaa = 'Jemin';
 // aaa += ' Patel';
 // aaa = aaa + ' Patel';
